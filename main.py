0867a1c67b068b7cd591b9a7378e3c4c3f82ed3c
Pavlyuchenko <mmichal.pavlicek@seznam.cz>
2020-12-06 11:56:03 +0100

    First Flask

--- main.py
+++ main.py
@@ -1,34 +1,89 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from datetime import datetime
 
 
-app = FastAPI()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "ioF45sdfFef84egfgew84aSgGDAGge84DGQEWgGd"
+db = SQLAlchemy(app)
 
-db = []
-
-class City(BaseModel):
-    name: str
-    timezone: str
-
-
-@app.get('/')
-def get_cities():
-    return db
+tags = db.Table(
+    "tags",
+    db.Column("clanek_id", db.Integer, db.ForeignKey("clanek.id")),
+    db.Column("stitek_id", db.Integer, db.ForeignKey("stitek.id")),
+)
 
 
-@app.get('/cities/{city_id}')
-def get_city(city_id: int):
-    return db[city_id]
+class Clanek(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    titulek = db.Column(db.String(1000), unique=False, nullable=False)
+    body = db.Column(db.Text, unique=True, nullable=False)
+    main_image = db.Column(db.String(9999), nullable=False)
+
+    datum = db.Column(db.DateTime, default=datetime.utcnow)
+
+    autor = db.Column(db.Integer, db.ForeignKey("autor.id"))
+    stitky = db.relationship(
+        "Stitek", secondary=tags, backref=db.backref("stitky", lazy="dynamic")
+    )
+
+    def __repr__(self):
+        return "<Clanek %r>" % self.titulek
 
 
-@app.post('/post')
-def post(city: City):
-    db.append(city.dict())
-    return 200
+class Autor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    jmeno = db.Column(db.String(1000), unique=False, nullable=False)
+
+    clanek = db.relationship("Clanek", backref="author")
+
+    def __repr__(self):
+        return self.jmeno
 
 
-@app.delete('/delete')
-def delete(city: int):
-    db.remove(db[city])
+class Stitek(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    nazev = db.Column(db.String(100), unique=True, nullable=False)
+    rubrika = db.Column(db.Boolean, default=False)
+    color = db.Column(db.String(10), nullable=False)
+
+    clanky = db.Column(db.Integer, db.ForeignKey("clanek.id"))
+
+    def __repr__(self):
+        return self.nazev
 
 
+class Rychlovka(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    titulek = db.Column(db.String(200), unique=False, nullable=False)
+    body = db.Column(db.Text, unique=True, nullable=False)
+
+
+class NewClanekView(ModelView):
+    # edit_template = "test.html"
+
+    column_exclude_list = ["main_image"]
+    column_searchable_list = ["titulek"]
+    column_filters = ["datum"]
+    column_editable_list = ["titulek"]
+    can_export = True
+
+
+admin = Admin(app, name="FotbalPropal.cz", base_template="admin/base.html")
+
+admin.add_view(NewClanekView(Clanek, db.session))
+admin.add_view(ModelView(Rychlovka, db.session))
+admin.add_view(ModelView(Autor, db.session))
+admin.add_view(ModelView(Stitek, db.session))
+
+
+if __name__ == "__main__":
+    app.run()
